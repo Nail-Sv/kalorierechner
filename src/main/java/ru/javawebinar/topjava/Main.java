b7a9efddbc2223fe9ab0cb4d379084d6bfcccefa
Nail-Sv <nail.sv86@gmail.com>
2022-06-02 21:54:02 +0200

    initial commit

---
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava;
+
+/**
+ * @see <a href="http://***.herokuapp.com">Demo application</a>
+ * @see <a href="https://github.com/Nail-Sv/">Initial project</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Hello TopJava Enterprise!");
+    }
+}
